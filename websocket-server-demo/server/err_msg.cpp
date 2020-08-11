c2292d434c294bfc274833caf6aed44860b531da
Harut Grigoryan <harut.grigoryan@4r-soft.com>
2020-08-11 16:23:35 +0400

    Add PCSC class to the project

---
+++ websocket-server-demo/server/err_msg.cpp
@@ -0,0 +1,53 @@
+// based on https://docs.microsoft.com/en-us/windows/win32/seccrypto/retrieving-error-messages
+
+#include <windows.h>
+#include <stdio.h>
+
+// Get error message text, given an error code.
+// Typically, the dwErr parameter passed to this function is retrieved
+// from GetLastError().
+
+void GetCSBackupAPIErrorMessage(DWORD dwErr, char * wszMsgBuff)
+{
+
+	DWORD   dwChars;  // Number of chars returned.
+
+	// Try to get the message from the system errors.
+	dwChars = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dwErr,
+		0,
+		wszMsgBuff,
+		512,
+		NULL);
+
+	if (0 == dwChars)
+	{
+		// The error code did not exist in the system errors.
+		// Try Ntdsbmsg.dll for the error code.
+
+		HINSTANCE hInst;
+
+		// Load the library.
+		hInst = LoadLibrary("Ntdsbmsg.dll");
+		if (NULL == hInst)
+		{
+			printf("cannot load Ntdsbmsg.dll\n");
+			exit(1);  // Could 'return' instead of 'exit'.
+		}
+
+		// Try getting message text from ntdsbmsg.
+		dwChars = FormatMessage(FORMAT_MESSAGE_FROM_HMODULE |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			hInst,
+			dwErr,
+			0,
+			wszMsgBuff,
+			512,
+			NULL);
+
+		// Free the library.
+		FreeLibrary(hInst);
+	}
+}
